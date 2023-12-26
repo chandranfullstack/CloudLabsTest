2bdac7e40f9a80fec6f7f29a15ae30c9e68aa359
chandranfullstack <ravichan997@gmail.com>
2023-12-26 14:02:42 +0530

    design

--- src/App.js
+++ src/App.js
@@ -4,37 +4,38 @@ import Popup from './Components/ui/popup';
 
 function App() {
   const [showPopup,setShowPopup]=useState(false)
-  const [segmentName, setSegmentName] = useState('');
-  const [selectedSchemas, setSelectedSchemas] = useState([]);
+  const [segmentName,setSegmentName]=useState('')
+  const [selectedSchemas,setSelectedSchemas]=useState([])
 
-  const schemaOptions = [
-    { label: 'First Name', value: 'first_name' },
-    { label: 'Last Name', value: 'last_name' },
-    { label: 'Gender', value: 'gender' },
-    { label: 'Age', value: 'age' },
-    { label: 'Account Name', value: 'account_name' },
-    { label: 'City', value: 'city' },
-    { label: 'State', value: 'state' },
-  ];
+  const schemaOptions=[
+    { label:"First Name",value:"first_name" ,type:"user"},
+    { label:"Last Name",value:"last_name" ,type:"user"},
+    { label:"Gender" , value: "gender",type:"user" },
+    { label:"Age",value:"age" ,type:"user"},
+    { label:"Account Name", value:"account_name" ,type:"group"},
+    { label:"City",value:"city" ,type:"group"},
+    { label:"State",value:"state",type:"group" }
+  ]
 
-  const handleAddSchema = (selectedSchema) => {
-    setSelectedSchemas([...selectedSchemas, selectedSchema]);
-  };
-
-  const handleSaveSegment = () => {
-    // Send data to the server here
-    const data = {
-      segment_name: segmentName,
-      schema: selectedSchemas.map((schema) => ({ [schema.value]: schema.label })),
-    };
-    console.log('Sending data to server:', data);
-  };
-  const handlePopup=()=>{
-    setShowPopup(true)
+  const handleAddSchema=(selectedSchema)=>{
+    setSelectedSchemas([...selectedSchemas,selectedSchema])
   }
+
+  const handleSaveSegment=()=>{
+    const data={
+      segment_name:segmentName,
+      schema:selectedSchemas?.map((s)=>({[s.value]:s.label}))
+    }
+  console.log(data,'data')
+  }
+
+  const handlePopup=()=>{
+    setShowPopup(!showPopup)
+  }
+
   return (
    <>
-   <div className=' container mx-auto bg-gray-400 h-screen backdrop-blur-lg '>
+   <div className=' container mx-auto bg-gray-700 bg-opacity-50 h-screen backdrop-blur-lg '>
      <div className=' flex justify-center items-center flex-row h-full'>
        <button className=' bg-transparent border border-4 border-[#FFFFFF] px-3 py-1 text-white' onClick={()=>handlePopup()}>
           Save segment
@@ -43,7 +44,7 @@ function App() {
      {
       showPopup&&(
         <Popup 
-        onClose={() => setShowPopup(false)}
+        onClose={()=>setShowPopup(false)}
         segmentName={segmentName}
         setSegmentName={setSegmentName}
         schemaOptions={schemaOptions}
--- src/Components/ui/popup.js
+++ src/Components/ui/popup.js
@@ -1,61 +1,108 @@
-// src/Popup.js
-import React, { useState } from 'react';
+import { useState } from 'react';
 
-const  Popup=({ onClose, segmentName, setSegmentName, schemaOptions, selectedSchemas, onAddSchema, onSaveSegment }) =>{
-  const [newSchema, setNewSchema] = useState('');
+const  Popup=({ onClose,segmentName,setSegmentName,schemaOptions,selectedSchemas,onAddSchema,onSaveSegment }) =>{
+  const [newSchema,setNewSchema]=useState('');
 
-  const handleAddNewSchema = () => {
-    if (newSchema && !selectedSchemas.some((schema) => schema.value === newSchema)) {
-      onAddSchema(schemaOptions.find((schema) => schema.value === newSchema));
-      setNewSchema('');
+  const handleAddNewSchema=()=>{
+    if(newSchema&&!selectedSchemas.some((s)=>s.value===newSchema)){
+        onAddSchema(schemaOptions.find((s)=>s.value===newSchema))
+        setNewSchema('')
     }
   };
 
   return (
-    <div className="fixed inset-0 bg-gray-700 bg-opacity-50 flex items-center justify-center">
-      <div className="bg-white p-8 rounded shadow-lg">
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Segment Name:</label>
-          <input
-            type="text"
-            className="border p-2 w-full"
-            value={segmentName}
-            onChange={(e) => setSegmentName(e.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Add Schema to Segment:</label>
-          <select
-            className="border p-2 w-full"
-            value={newSchema}
-            onChange={(e) => setNewSchema(e.target.value)}
-          >
-            <option value="" disabled>
-              Select an option
-            </option>
+    <>
+    <div className=' fixed right-0 top-0 z-[50]  h-full flex'>
+     <div className=' bg-white p-8'>
+       <div className=' mb-4'>
+        <label className=' inline-flex mb-4'>Enter the Name of the Segment</label>
+        <input 
+        type="text" 
+        placeholder='Name of the segment'
+        className=" w-full p-2 border border-gray-500 rounded focus:outline-none" 
+        value={segmentName} 
+        onChange={(e)=>setSegmentName(e?.target?.value)}
+        />
+        <p className=' w-[80%] mt-4'>
+          To save your segment, you need to add the schemas to build the query
+        </p> 
+       </div>
+
+       <div className=' my-4 flex justify-end'>
+         <div className=' flex justify-between gap-4'>
+           <p className=' flex items-center gap-1'><span className=' inline-flex items-center rounded-full bg-green-500 w-4 h-4'></span> - User Traits</p>
+           <p className=' flex items-center gap-1'><span className=' inline-flex items-center rounded-full w-4 h-4 bg-pink-500'></span> - Group Traits</p>
+         </div>
+       </div>
+
+       <div className=' my-2'>
+         {
+          selectedSchemas?.map((i)=>(
+            <>
+          <div className=' flex items-center gap-4 my-2 '>
+          <span className={`inline-flex w-4 h-4 ${i?.type==="user"?" bg-green-500":"bg-pink-500"} rounded-full`}></span>
+          <div className="relative inline-block text-left">
+          <select className="block  w-[400px] appearance-none  bg-white border border-gray-500 py-2 px-4 pr-8 rounded leading-tight focus:outline-none">
+              <option key={i.value} value={i.value}>
+                {i.label}
+              </option>
+          </select>
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 12l-8-8 1.5-1.5L10 9l6.5-6.5L18 4z"/></svg>
+          </div>
+          </div>
+          <div>
+            <button className=' p-4 bg-blue-200'>
+              <span className='flex w-6 h-1 rounded-md bg-gray-600'></span>
+            </button>
+          </div>
+         </div>
+            </>
+          ))
+         }
+       </div>
+       <div className='my-2 '>
+         <div className=' flex items-center gap-4 '>
+          <span className=' inline-flex w-4 h-4 bg-gray-300 rounded-full'></span>
+          <div className="relative inline-block text-left">
+          <select 
+           value={newSchema}
+           onChange={(e) => setNewSchema(e.target.value)}
+          className="block  w-[400px] appearance-none  bg-white border border-gray-500 py-2 px-4 pr-8 rounded leading-tight focus:outline-none">
+          <option value="" >
+          Add schema to segment            
+          </option>
             {schemaOptions.map((option) => (
               <option key={option.value} value={option.value}>
                 {option.label}
               </option>
             ))}
           </select>
-        </div>
-        <button className="bg-blue-500 text-white px-4 py-2 mb-4" onClick={handleAddNewSchema}>
-          + Add New Schema
-        </button>
-        {selectedSchemas.map((schema) => (
-          <div key={schema.value} className="mb-2">
-            {schema.label}
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 12l-8-8 1.5-1.5L10 9l6.5-6.5L18 4z"/></svg>
           </div>
-        ))}
-        <button className="bg-green-500 text-white px-4 py-2" onClick={onSaveSegment}>
-          Save Segment
-        </button>
-        <button className="ml-2 text-gray-700" onClick={onClose}>
-          Cancel
-        </button>
-      </div>
+          </div>
+          <div>
+            <button className=' p-4 bg-blue-200'>
+              <span className='flex w-6 h-1 rounded-md bg-gray-600'></span>
+            </button>
+          </div>
+         </div>
+       </div>
+       
+       <div className=' ml-[30px] mt-4'>
+        <button  onClick={()=>handleAddNewSchema()} className=' bg-transparen text-[#009F7F]'>+ <span className=' underline'>Add New Schema</span></button>
+       </div>
+       <div className=' bottom-0 absolute'>
+        <div className=' flex gap-4 p-5'>
+          <button onClick={onSaveSegment} className=' bg-[#009F7F] text-white p-2 rounded'>Save the Segment</button>
+          <button onClick={onClose} className=' bg-gray-100 text-pink-500 p-2 rounded'>Cancel</button>
+        </div>
+       </div>
+     </div>
     </div>
+    </>
+
   );
 }
 
